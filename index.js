3de9bfef9ff5c0ebd11334c940b6b0a0903ce486
yuseth <yusethnataly@gmail.com>
2021-09-24 00:45:49 -0400

    triangulo

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var calcularAreaCuadrado = () => {
     const resultado = areaDelCuadrado(ladoDelCuadrado)
     document.querySelector("#resultado").innerHTML = resultado
 }
+var Pi =Math.PI;
+const perimetroDelCirculo =(Radio)=> 2 * Pi *Radio;
+const areaDelCirculo =(Radio) => Pi*Radio^2;
+
+var CalcularAreaCirculo = () =>{
+    const Radio = document.querySelector("#Radio").value;
+    const resultado = areaDelCirculo(Radio)
+    document.querySelector("#resultado").innerHTML = resultado
+}
